7f8e0a2008894d7103c7f61b5947491a464e091c
Oleg Popko <goodmans.too@gmail.com>
2021-07-29 15:35:52 +0300

    delete old files

--- src/test/java/PARAMETERS.java
+++
@@ -1,6 +0,0 @@
-public final class PARAMETERS {
-    public final static String LOGIN = "O_P@mail.com";
-    public final static String PASSWORD = "123qwe";
-    public final static String DRIVER_PATH = "D:\\IdeaProjects\\chromedriver.exe";
-    public final static String FILE_PATH = "D:\\2021-06-25_21-21.png";
-}

62cb717b4f6d898fddced744d558931884469790
Oleg Popko <goodmans.too@gmail.com>
2021-07-29 15:36:22 +0300

    update parameters file

---
+++ src/test/java/properties/PARAMETERS.java
@@ -0,0 +1,8 @@
+package properties;
+
+public final class PARAMETERS {
+    public final static String LOGIN = "O_P@mail.com";
+    public final static String PASSWORD = "123qwe";
+    public final static String DRIVER_PATH = "src/main/resources/chromedriver.exe";
+    public final static String FILE_PATH = "";
+}
